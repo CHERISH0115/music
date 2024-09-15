4814f69ee4c1259850e7bb083c5b7be923c60528
CHERISH <1342620995@qq.com>
2024-09-15 15:40:25 +0800

    Update example.js

--- src/example.js
+++ src/example.js
@@ -1,10 +1,5 @@
 export default {
     "netease": {
         "playlist": { "show": true, "value": "5356348587" },
-        "song": { "show": true, "value": "473403185" },
-        "artist": { "show": true, "value": "12441107" },
-        "lrc": { "show": false, "value": "2015217630" },
-        "url": { "show": false, "value": "473403185" },
-        "search": { "show": true, "value": "KN33S0XXX" },
     }
 }
